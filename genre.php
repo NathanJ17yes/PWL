d03efb4716256fefaca8ad23b77639ae4472e7c7
adhitptra <91391759+adhitptra@users.noreply.github.com>
2023-03-04 18:24:36 +0700

    Update genre.php

--- genre.php
+++ genre.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Genre</title>
     <link rel="stylesheet" href = "genre.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
    <?php
@@ -19,7 +21,7 @@
         $link = null;
     ?>
     <main>
-        <table>
+        <table class="table table-hover">
             <tr>
                 <th>ID</th>
                 <th>Name</th>
@@ -36,7 +38,5 @@
             </tbody>
         </table> 
     </main>
-
 </body>
 </html>
-
