fe8ffb0a261a30eb2831b8c3e83b9193b31e93b8
Fabio Jovancha <90676286+jvnzzy@users.noreply.github.com>
2023-03-04 18:25:38 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,17 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UltraBook Center</title>
-    <link rel="stylesheet" href = "index.css">
+    <title>home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <nav id= "navbar">
-        <ul>
-            <li><a href="?menu=home">home</a></li>
-            <li><a href="?menu=genre">genre</a></li>
-            <li><a href="?menu=book">book</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">UltraBook</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="?menu=home">home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?menu=genre">genre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?menu=book">book</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
     <main>
         <?php
